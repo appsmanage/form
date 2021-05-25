ea0654477132b33b095738b711ee861f993125ba
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-05-25 19:50:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(function () {
         var username = $('input[name="username"]').val();
         var email = $('input[name="email"]').val();
         //var mes_date = $('textarea[name="end_time"]').val();
-        var plan = $('input[name="plan"]').val();
+        var plan = $('input[name="plan"]:checked').val();
         var msg = `${key}\nお名前：${username}\nEmail：${email}\nお申込みプラン：${plan}`;
         sendText(msg, plan);
 

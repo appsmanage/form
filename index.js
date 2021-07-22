4a95e9c06a9df07d21d34b100103aa2b3b4b49f5
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-07-22 23:49:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var params = (new URL(document.location)).searchParams;
-var key = params.get('key');
 
 $(function () {
     // 送信
@@ -9,7 +7,7 @@ $(function () {
         //var mes_date = $('textarea[name="end_time"]').val();
         var plan = $('input[name="plan"]:checked').val();
         var msg = `利用申請フォーム\nお名前：${username}\nEmail：${email}\nお申込みプラン：${plan}`;
-        sendText(msg, plan);
+        sendText(msg);
 
         return false;
     });

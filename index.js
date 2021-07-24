a7f065afa19dc8aa4d0d233c6cdc6482282d00c3
yusuke.jindo <yusuke.j0728@gmail.com>
2021-07-24 11:15:13 +0900

    edit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// // urlの末尾に?=をつけてkeyを追加することができる
+// var params = (new URL(document.location)).searchParams;
+// var key = params.get('key');
 
 $(function () {
     // 送信
@@ -7,6 +10,7 @@ $(function () {
         //var mes_date = $('textarea[name="end_time"]').val();
         var plan = $('input[name="plan"]:checked').val();
         var msg = `利用申請フォーム\nお名前：${username}\nEmail：${email}\nお申込みプラン：${plan}`;
+        // sendText(msg, plan);
         sendText(msg);
 
         return false;
